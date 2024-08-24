bf23845b47da8077068ecbbf80f5099dea6ef98f
seetharam <ravivijay2306@gmail.com>
2024-08-24 09:28:21 +0530

    app.js file updated

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello!</h1>
+    <div className="calculator">
+        <input type="text" className="calculator-screen" id="screen" readonly></input>
+        
+        <div className="calculator-keys">
+            <button type="button" className="operator" value="+">+</button>
+            <button type="button" className="operator" value="-">–</button>
+            <button type="button" className="operator" value="*">&times;</button>
+            <button type="button" className="operator" value="/">&divide;</button>
+
+            <button type="button" value="7">7</button>
+            <button type="button" value="8">8</button>
+            <button type="button" value="9">9</button>
+
+            <button type="button" value="4">4</button>
+            <button type="button" value="5">5</button>
+            <button type="button" value="6">6</button>
+
+            <button type="button" value="1">1</button>
+            <button type="button" value="2">2</button>
+            <button type="button" value="3">3</button>
+
+            <button type="button" value="0">0</button>
+            <button type="button" className="decimal" value=".">.</button>
+            <button type="button" className="all-clear" value="all-clear">AC</button>
+
+            <button type="button" className="equal-sign operator" value="=">=</button>
+        </div>
     </div>
   );
 }
